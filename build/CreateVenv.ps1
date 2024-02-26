161d81a03f3c3529908c92bf42935d3b410f2bdd
Marius-Adrian Toma <marius.toma@innoviz-tech.com>
2024-02-26 22:24:01 +0200

    Correct CreateVenv.ps1 script

--- build/CreateVenv.ps1
+++ build/CreateVenv.ps1
@@ -3,7 +3,7 @@ $envName = 'py_virtual_env'
 $fullEnvPath = "$PSScriptRoot\$envName"
 $activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
 Out-Host -InputObject("Creating python venv: $fullEnvPath")
-python.exe -m venv $envName
+python.exe -m venv $fullEnvPath
 & $activateScriptPath
 #upgrade pip
 python -m pip install --upgrade pip
