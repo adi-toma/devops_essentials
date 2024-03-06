40b375a7070a887eb43cec64a8d9acbdff44bb8f
Lorena Berchesan <lorenaberchesan@yahoo.com>
2024-03-06 12:01:28 +0200

    build commit

---
+++ build/local_repobuildCreateVenv.ps1
@@ -0,0 +1,19 @@
+[11:58 AM] Marius Toma
+$envName = 'py_virtual_env'
+$fullEnvPath = "$PSScriptRoot\$envName"
+$activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
+ 
+Out-Host -InputObject("Workspace env variable: {0}" -f $env:WORKSPACE)
+Out-Host -InputObject("Creating python venv: $fullEnvPath")
+python.exe -m venv $fullEnvPath
+ 
+# activate the Python environment
+& $activateScriptPath
+ 
+#upgrade pip
+python -m pip install --upgrade pip
+pip list
+ 
+deactivate
+ 
+Out-Host -InputObject("exit code: {0}" -f $LASTEXITCODE)
