e1edd23d08c38da056d3698c59b45f9005351545
RaresSuflet <rares.suflet@gmail.com>
2024-03-07 12:02:56 +0200

    Update to latest version

--- build/Invoke-Flake8.ps1
+++ build/Invoke-Flake8.ps1
@@ -1,8 +1,11 @@
 $envName = 'py_virtual_env'
 $fullEnvPath = "$PSScriptRoot\$envName"
 $activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
+$pythonSourcePath = "$env:WORKSPACE\greetings"
 
 # activate the Python environment
-& $activateScriptPath
+& $activateScriptPath 
+
+flake8 $pythonSourcePath --output-file=flake8_results.txt
 
 deactivate
