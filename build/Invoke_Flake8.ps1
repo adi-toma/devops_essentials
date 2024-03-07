e6640d912e451b2cf27b336642cbe36eda71d60c
Sandu Smaranda <smara1609@gmail.com>
2024-03-07 12:05:12 +0200

    modify flake8

--- build/Invoke_Flake8.ps1
+++ build/Invoke_Flake8.ps1
@@ -7,5 +7,6 @@ $pythonSourcePath = "$env:WORKSPACE\greetings"
 & $activateScriptPath
 
 flake8 $pythonSourcePath --output-file=flake8_results.txt
+# ckeck exit code of script and print exit code and file contents if non zero exit
 
 deactivate
