b2b240d7af3bcc59f94d16b082977a57126f2592
Lorena Berchesan <lorenaberchesan@yahoo.com>
2024-03-07 11:12:21 +0200

    Addede stage for flake8 check + script

---
+++ build/Invoke-Flake8.ps1
@@ -0,0 +1,6 @@
+$envName = 'py_virtual_env'
+$fullEnvPath = "$PSScriptRoot\$envName"
+$activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
+
+#activate the Python environment
+& $activateScriptPath
