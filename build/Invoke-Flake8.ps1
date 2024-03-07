d49623377fed891d13f655bff05f841b9cd0a1a2
Biceada Stefana <91457306+StefanaInf@users.noreply.github.com>
2024-03-08 00:46:35 +0200

    fixed

--- build/Invoke-Flake8.ps1
+++ build/Invoke-Flake8.ps1
@@ -1,7 +1,7 @@
 $envName = 'py_virtual_env'
 $fullEnvPath = "$PSScriptRoot\$envName"
 $activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
-$pythonSourcePath = "$env:WORKSPACE\greetings"
+
 
 # activate the Python environment
 & $activateScriptPath 
