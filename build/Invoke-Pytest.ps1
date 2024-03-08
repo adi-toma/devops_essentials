7c88b79a8a67d665a7e0a67c65ce0fd9531503c6
Buzgau Catalin <buzgaucatalin@yahoo.com>
2024-03-08 11:43:00 +0200

    Refactor script

--- build/Invoke-Pytest.ps1
+++ build/Invoke-Pytest.ps1
@@ -7,7 +7,7 @@ $pythonSourcePath = "$env:WORKSPACE\greetings"
 
 #change current working directory
 Push-Location -Path $pythonSourcePath
-pytest --cov=$pythonSourcePath --cov-report html
+pytest --html=report.html
 Pop-Location
 
 deactivate
