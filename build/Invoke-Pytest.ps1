b989787e6cac9b0861f8817d537ebaf7c1f68b9b
Buzgau Catalin <buzgaucatalin@yahoo.com>
2024-03-08 11:32:51 +0200

    Save code coverage to html

--- build/Invoke-Pytest.ps1
+++ build/Invoke-Pytest.ps1
@@ -7,7 +7,7 @@ $pythonSourcePath = "$env:WORKSPACE\greetings"
 
 #change current working directory
 Push-Location -Path $pythonSourcePath
-pytest
+pytest --cov-report html
 Pop-Location
 
 deactivate
