edbc6f0e6335c5cfa7cef2abcd0eb3598391da80
Diana Panainte <panaintediana33@gmail.com>
2024-03-08 10:49:39 +0200

    created invoke-pytest file

--- build/CreateVenv.ps1
+++ build/CreateVenv.ps1
@@ -12,6 +12,9 @@ C:\Users\panai\AppData\Local\Programs\Python\Python311\python.exe -m venv $fullE
 #upgrade pip
 python -m pip install --upgrade pip
 pip install flake8
+pip install pytest
+pip install pyyaml
+pip install nose
 
 pip list
 
