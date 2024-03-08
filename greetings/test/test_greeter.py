c8e23a907dbeff23f029c566d5caca005fe69a55
GGabriel <gheorghiu.gaby8@gmail.com>
2024-03-08 11:14:25 +0200

    replace yml.load with yml.safe.load

--- greetings/test/test_greeter.py
+++ greetings/test/test_greeter.py
@@ -7,7 +7,7 @@ from nose.tools import assert_equal
 def test_greeter():
     with open(os.path.join(os.path.dirname(__file__),
                            'fixtures', 'samples.yaml')) as fixtures_file:
-        fixtures = yaml.load(fixtures_file)
+        fixtures = yaml.safe.load(fixtures_file)
         for fixture in fixtures:
             answer = fixture.pop('answer')
             assert_equal(greet(**fixture), answer)
