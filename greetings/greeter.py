75d362ee7778621ddd1015bca1ff4f5671bd1e19
Buzgau Catalin <buzgaucatalin@yahoo.com>
2024-03-08 11:28:52 +0200

    Refactor code

--- greetings/greeter.py
+++ greetings/greeter.py
@@ -21,6 +21,5 @@ def greet(personal, family, title="", polite=False):
     greeting = "How do you do, " if polite else "Hey, "
     if title:
         greeting += title + " "
-        greeting += title+" "
     greeting += personal + " " + family + "."
     return greeting
