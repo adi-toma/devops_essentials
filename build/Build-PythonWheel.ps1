4268f9dcdddb2fa2428c63bb4647dd965631bbaf
Marius-Adrian Toma <marius.toma@innoviz-tech.com>
2024-03-10 16:17:03 +0200

    Simplify script

--- build/Build-PythonWheel.ps1
+++ build/Build-PythonWheel.ps1
@@ -1,12 +1,3 @@
-<#
-    Script to build the Python wheel
-    Prerequisites: $env:WORKSPACE env variable is needed
-#>
-param (
-    # name of the output image
-    [string]$buildParam = 'clean'
-)
-
 $envName = 'py_virtual_env'
 $fullEnvPath = "$PSScriptRoot\$envName"
 $activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
@@ -16,7 +7,7 @@ $activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
 
 #change current working directory to the root of the repository
 Push-Location $env:WORKSPACE
-python setup.py $buildParam
+python setup.py bdist
 Pop-Location
 
 deactivate
