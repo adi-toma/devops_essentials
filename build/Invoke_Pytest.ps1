8064acb3b09dc32f749563204cc3c4a249b15b56
Sandu Smaranda <smara1609@gmail.com>
2024-03-10 21:13:41 +0200

    python test updates

---
+++ build/Invoke_Pytest.ps1
@@ -0,0 +1,14 @@
+$envName = 'py_virtual_env'
+$fullEnvPath = "$PSScriptRoot\$envName"
+$activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
+$pythonSourcePath = "$env:WORKSPACE\greetings"
+
+# activate the Python environment
+& $activateScriptPath
+
+#create current working directory
+Push-Location $pythonSourcePathToCheck
+pytest --html=report.html --self-contained-html
+Pop-Location
+
+deactivate
