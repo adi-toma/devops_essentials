ced6a485d921fe55a4f3f8195744d2da39756884
biancalarisabud <72990964+biancalarisabud@users.noreply.github.com>
2024-03-11 23:35:51 +0200

    Add files via upload

---
+++ .github/workflows/CreateVenv.ps1
@@ -0,0 +1,16 @@
+$envName = 'py_virtual_env'
+$fullEnvPath = "$PSScriptRoot\$envName"
+$activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
+
+Out-Host -InputObject("Creating python venv: $fullEnvPath")
+python.exe -m venv $fullEnvPath
+
+#activate the Python environment
+& $activateScriptPath
+
+#upgrade pip
+python -m pip install --upgrade pip
+pip list
+deactivate
+
+Out-Host -InputObject("exit code: {0}" -f $LASTEXITCODE)
