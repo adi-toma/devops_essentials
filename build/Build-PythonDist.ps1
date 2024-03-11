1ec5373d8358e682bb1e51cee0d50959cdbf35bf
Diana Panainte <diana.panainte@student.uaic.ro>
2024-03-11 12:01:15 +0200

    modified build-pythonDist

--- build/Build-PythonDist.ps1
+++ build/Build-PythonDist.ps1
@@ -1,11 +1,12 @@
 $envName = 'py_virtual_env'
 $fullEnvPath = "$PSScriptRoot\$envName"
 $activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
+$pythonSourcePath = "$env:WORKSPACE\python"
 
 # activate the Python environment
 & $activateScriptPath
 
-Push-Location "$env:WORKSPACE\python"
+Push-Location $pythonSourcePath
 
 python.exe setup.py sdist
 
