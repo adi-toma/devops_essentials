06c8aff7aa813a916cdb4021a2c205629f279ca7
GGabriel <gheorghiu.gaby8@gmail.com>
2024-03-11 11:42:46 +0200

    Added archiveArtifacts.gz

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     version = "0.1.0",
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/greet'],
-    install_requires = ['argparse']
+    install_requires = ['argparse'],
+    author_email="author@gmail.com"
 )
