091238efdbc007d3202a0ba72b5f69f6e51e41af
Biceada Stefana <91457306+StefanaInf@users.noreply.github.com>
2024-03-11 10:17:37 +0200

    day6

--- build/CreateVenv.ps1
+++ build/CreateVenv.ps1
@@ -12,7 +12,10 @@ python.exe -m venv $fullEnvPath
 #upgrade pip
 python -m pip install --upgrade pip
 pip install flake8
-
+pip install pytest
+pip install pyyaml
+pip install nose
+pip install pytest-html
 pip list
 
 deactivate
