e34e280f2a72cbd406b7abf7a850da13a391611c
Lorena Berchesan <lorenaberchesan@yahoo.com>
2024-03-11 11:18:08 +0200

    commit Build-PythonDist.ps1

---
+++ build/Build-PythonDist.ps1
@@ -0,0 +1,12 @@
+$envName = 'py_virtual_env'
+$fullEnvPath = "$PSScriptRoot\$envName"
+$activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
+
+#activate the Python environment
+& $activateScriptPath
+
+Push-Location $env:WORKSPACE
+
+python.exe setup.py sdist
+
+deactivate
