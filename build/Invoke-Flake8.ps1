5638b945e59a1850128148d296b3e505db77cd82
Marius-Adrian Toma <marius.toma@innoviz-tech.com>
2024-03-12 10:23:39 +0200

    Fix flake8 scripting

--- build/Invoke-Flake8.ps1
+++ build/Invoke-Flake8.ps1
@@ -1,11 +1,11 @@
 $envName = 'py_virtual_env'
 $fullEnvPath = "$PSScriptRoot\$envName"
 $activateScriptPath = "$fullEnvPath\Scripts\Activate.ps1"
-$pythonSourcePath = "$env:WORKSPACE\greetings"
+$pythonSourcePath = "$env:WORKSPACE\python\greetings"
 
 # activate the Python environment
 & $activateScriptPath 
 
 flake8 $pythonSourcePath --output-file=flake8_results.txt
-
+# check exit code of script and print exit code and file contents if non zero exit code
 deactivate
